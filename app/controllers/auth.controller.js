ae7bd3c4dde8d78116078f85df67b05f9f6e52ab
kompaniietst <kompaniets.t@gmail.com>
2021-12-12 20:25:23 +0200

    /

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -103,48 +103,48 @@ exports.signup = (req, res) => {
   });
 };
 
-// exports.signin = (req, res) => {
-//   User.findOne({
-//     email: req.body.email
-//   })
-//     .populate("roles", "-__v")
-//     .exec((err, user) => {
-//       if (err) {
-//         res.status(500).send({ message: err });
-//         return;
-//       }
+exports.signin = (req, res) => {
+  User.findOne({
+    email: req.body.email
+  })
+    .populate("roles", "-__v")
+    .exec((err, user) => {
+      if (err) {
+        res.status(500).send({ message: err });
+        return;
+      }
 
-//       if (!user) {
-//         return res.status(404).send({ message: "User Not found." });
-//       }
+      if (!user) {
+        return res.status(404).send({ message: "User Not found." });
+      }
 
-//       var passwordIsValid = bcrypt.compareSync(
-//         req.body.password,
-//         user.password
-//       );
+      var passwordIsValid = bcrypt.compareSync(
+        req.body.password,
+        user.password
+      );
 
-//       if (!passwordIsValid) {
-//         return res.status(401).send({
-//           token: null,
-//           message: "Invalid Password!"
-//         });
-//       }
+      if (!passwordIsValid) {
+        return res.status(401).send({
+          token: null,
+          message: "Invalid Password!"
+        });
+      }
 
-//       var token = jwt.sign({ id: user.id }, config.secret, {
-//         expiresIn: 86400 // 24 hours
-//       });
+      var token = jwt.sign({ id: user.id }, config.secret, {
+        expiresIn: 86400 // 24 hours
+      });
 
-//       var authorities = [];
+      var authorities = [];
 
-//       for (let i = 0; i < user.roles.length; i++) {
-//         authorities.push("ROLE_" + user.roles[i].name.toUpperCase());
-//       }
-//       res.status(200).send({
-//         id: user._id,
-//         username: user.username,
-//         email: user.email,
-//         roles: authorities,
-//         token: token
-//       });
-//     });
-// };
+      for (let i = 0; i < user.roles.length; i++) {
+        authorities.push("ROLE_" + user.roles[i].name.toUpperCase());
+      }
+      res.status(200).send({
+        id: user._id,
+        username: user.username,
+        email: user.email,
+        roles: authorities,
+        token: token
+      });
+    });
+};
