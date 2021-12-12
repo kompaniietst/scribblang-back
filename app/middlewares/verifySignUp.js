aa0af64314c8757a0f46613528366265461aa322
kompaniietst <kompaniets.t@gmail.com>
2021-12-12 20:31:47 +0200

    d

--- app/middlewares/verifySignUp.js
+++ app/middlewares/verifySignUp.js
@@ -52,8 +52,8 @@
 // };
 
 const verifySignUp = {
-    // checkDuplicateUsernameOrEmail,
-    // checkRolesExisted
+    checkDuplicateUsernameOrEmail,
+    checkRolesExisted
 };
 
 // module.exports = verifySignUp;
