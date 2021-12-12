1da91109e0eed506b332cad2b6688f3088738521
kompaniietst <kompaniets.t@gmail.com>
2021-12-12 20:28:42 +0200

    .

--- app/routes/auth.routes.js
+++ app/routes/auth.routes.js
@@ -13,11 +13,11 @@ module.exports = function (app) {
     app.post(
         "/api/auth/signup",
         [
-            // verifySignUp.checkDuplicateUsernameOrEmail,
-            // verifySignUp.checkRolesExisted
+            verifySignUp.checkDuplicateUsernameOrEmail,
+            verifySignUp.checkRolesExisted
         ],
         controller.signup
     );
 
-    // app.post("/api/auth/signin", controller.signin);
+    app.post("/api/auth/signin", controller.signin);
 };
