d47d638b6a79cd4bffea9db5b112b9fcc6fba293
kompaniietst <kompaniets.t@gmail.com>
2021-12-12 19:58:21 +0200

    .

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  // .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 // parse requests of content-type - application/json
